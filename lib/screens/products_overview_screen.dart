98a2461f209ff293a01a115fafb42abd6e77bb8f
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2021-08-14 15:56:35 +0200

    first commit

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  const ProductsOverviewScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop'),
+      ),
+    );
+  }
+}
